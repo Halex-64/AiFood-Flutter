ccd8f183d8e93701b8cd39c104c849edf520664b
Halex-64 <halexgoncalves6@gmail.com>
2024-02-22 11:03:20 -0300

    Continuando telainicio.dart

--- lib/Screens/TelaInicio/telainicio.dart
+++ lib/Screens/TelaInicio/telainicio.dart
@@ -15,7 +15,10 @@ class _TelaInicioState extends State<TelaInicio> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('R. Ari Barroso, 330', style: textNunitobold(14),),
+        title: Text(
+          'R. Ari Barroso, 330',
+          style: textNunitobold(14),
+        ),
         centerTitle: true,
         actions: const [
           Padding(
@@ -24,27 +27,41 @@ class _TelaInicioState extends State<TelaInicio> {
           )
         ],
       ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: Row(
-              children: [
-                card(185, 80, const Color.fromARGB(255, 95, 15, 133), 'Restau\nrantes', 'assets/peixes.png'),
-                card(185, 80, const Color.fromARGB(255, 95, 15, 133), 'Mercado', 'assets/mercado.png'),
-              ],
+      body: ListView(children: [
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Row(
+            children: [
+              card(185, 80, const Color.fromARGB(255, 95, 15, 133),
+                  'Restau\nrantes', 'assets/peixes.png'),
+              card(185, 80, const Color.fromARGB(255, 95, 15, 133), 'Mercado',
+                  'assets/mercado.png'),
+            ],
+          ),
+        ),
+        Row(
+          children: [
+            cardDois(90, 100, const Color.fromARGB(255, 195, 170, 238),
+                'Farmácia', 'assets/farmacia.png'),
+            cardDois(90, 100, const Color.fromARGB(255, 195, 170, 238),
+                'Bebidas', 'assets/bebidas.png'),
+            const SizedBox(
+              width: 10,
             ),
-          ),
-            Row(
-              children: [
-                cardDois(90, 100, const Color.fromARGB(255, 195, 170, 238), 'Farmácia', 'assets/farmacia.png'),
-                cardDois(90, 100, const Color.fromARGB(255, 195, 170, 238), 'Bebidas', 'assets/bebidas.png'),
-                const SizedBox(width: 10,),
-                cardDois(90, 100, const Color.fromARGB(255, 195, 170, 238), 'Sucos', 'assets/sucos.png'),
-                cardDois(90, 100, const Color.fromARGB(255, 195, 170, 238), 'Pizza', 'assets/pizza.png'),
-              ],
-          ),
-          const Slide(),
+            cardDois(90, 100, const Color.fromARGB(255, 195, 170, 238), 'Sucos',
+                'assets/sucos.png'),
+            cardDois(90, 100, const Color.fromARGB(255, 195, 170, 238), 'Pizza',
+                'assets/pizza.png'),
+          ],
+        ),
+        const Slide(),
+        espacoVertical(24),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child:
+              Text('Para você que gosta de cupom', style: textRobotobold(16)),
+        ),
+        cardDois(50, 0, const Color.fromARGB(255, 190, 179, 179), 'Em Mercados', 'assets/mercado.png'),
       ]),
     );
   }
